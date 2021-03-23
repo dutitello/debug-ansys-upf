bb271bc06054667d0b0481c443c6a65dc5488204
dutitello <du.titello@gmail.com>
2021-03-23 08:40:03 -0300

    original v195 files

---
+++ bats/debug_anscust_v195.bat
@@ -0,0 +1,289 @@
+@echo off
+cls
+
+rem  These need to change, for each new release.  You might need to change
+rem  the SetParameters function, if the compiler changes.  Otherwise, the
+rem  rest of the script should not need changing.
+rem
+set "revndot=19.5"
+set "revncust=2019 R3"
+set "ROOT_DIR=%AWP_ROOT195%"
+set "VSVER=vs2017"
+set "IFORT_HOME=%IFORT_COMPILER17%"
+set "VCINPATH=%VS2017INSTALLDIR%\VC\Tools\MSVC\14.10.25017"
+
+rem  Yes, we are saving the current working directory, and we chdir back
+rem  into it, at various points in this script.  Normally, this script is
+rem  run from a cmd.exe prompt, and it works fine without this.  But, we
+rem  have automated test scripts, that call this script, and things fail,
+rem  if we don't explicitly move back into the working directory.
+set "ACWORKDIR=%cd%"
+
+
+rem :::::::::::::::::::::::::::::::
+rem ::                           ::
+rem ::     E X E C U T I V E     ::
+rem ::                           ::
+rem :::::::::::::::::::::::::::::::
+
+call :SetParameters  ||  goto :END
+call :ShowBanner     ||  goto :END
+call :AskAboutAero   ||  goto :END
+call :CompileStuff   ||  goto :END
+call :LinkStuff      ||  goto :END
+call :CopyDlls       ||  goto :END
+
+goto :END
+
+
+rem :::::::::::::::::::::::::::::::
+rem ::                           ::
+rem ::     F U N C T I O N S     ::
+rem ::                           ::
+rem :::::::::::::::::::::::::::::::
+
+:SetParameters
+   set "PLATFORM_DIR=winx64"
+   set "IFORT_PLATFORM=intel64"
+   set "BIT_TARGET=64"
+
+   call "%IFORT_HOME%\bin\compilervars.bat" %IFORT_PLATFORM% %VSVER%
+
+   set "INCLUDE=%ROOT_DIR%\ansys\customize\Include;%INCLUDE%"
+   set "LIB=%ROOT_DIR%\ansys\Custom\Lib\%PLATFORM_DIR%;%LIB%"
+   exit /B 0
+
+
+:ShowBanner
+   echo.
+   echo.   ***********************************************************************
+   echo.
+   echo.   This is the Mechanical APDL revision %revncust% ANSCUST batch file.  It is
+   echo.   used to link User Programmable Features, into versions of the M-APDL
+   echo.   program, on Microsoft Windows %BIT_TARGET%-bit systems.
+   echo.
+   echo.                   ******   IMPORTANT !!!! ******
+   echo.
+   echo.   The provided user subroutine source files reside in this folder:
+   echo.      {InstallDir}\customize\user\
+   echo.
+   echo.   Please copy the source file^(s^), that you wish to modify, from the above
+   echo.   folder, into your working folder, to include them in your link.  ^(The
+   echo.   default working folder is {InstallDir}\custom\user\%PLATFORM_DIR%\ .^)
+   echo.
+   echo.   If you want to use a different working folder, then also copy these other
+   echo.   files, from the default working folder, to your actual working folder:
+   echo.
+   echo.      ANSCUST.BAT
+   echo.      ansysex.def
+   echo.      ansys.lrf files
+   echo.      app.manifest
+   echo.
+   echo.   When you are done copying, run ANSCUST.BAT from your working folder.
+   echo.
+   echo.   ***********************************************************************
+   pause
+   exit /B 0
+
+
+:AskAboutAero
+   set "AERO=FALSE"
+   set /A ECNT=10
+
+:AAA1
+   echo.
+   set /P ANSW=Do you want to link the Wind Turbine Aeroelastic library with Mechanical APDL? (Y or N):
+
+   if /I "%ANSW%"=="Y" (
+      set "AERO=TRUE"
+      exit /B 0
+
+   ) else if /I "%ANSW%"=="N" (
+      set "AERO=FALSE"
+      exit /B 0
+
+   ) else (
+      set /a ECNT=ECNT-1
+
+      if "%ECNT%"=="0"  (
+         echo.
+         echo. giving up ...
+         echo.
+         exit /B 1
+      )
+
+      echo.
+      echo. Please answer Y or N
+      echo.
+   )
+
+   goto :AAA1
+
+
+:CompileStuff
+   setlocal EnableDelayedExpansion
+   cd "%ACWORKDIR%"
+
+   del /q compile.log compile_error.txt  >NUL 2>&1
+
+   if exist *.obj      ( del /Q *.obj      >NUL 2>&1 )
+   if exist ANSYS.exe  ( del /Q ANSYS.exe  >NUL 2>&1 )
+   if exist ANSYS.exp  ( del /Q ANSYS.exp  >NUL 2>&1 )
+   if exist ANSYS.lib  ( del /Q ANSYS.lib  >NUL 2>&1 )
+   if exist ANSYS.map  ( del /Q ANSYS.map  >NUL 2>&1 )
+
+   set "CUSTMACROS=/DNOSTDCALL /DARGTRAIL /DPCWIN64_SYS /DPCWINX64_SYS /DPCWINNT_SYS /DCADOE_ANSYS"
+   set "CMACS=/DCURVEFIT_EXPORTS /D_X86=1 /DOS_WIN32 /DWIN32 /D__STDC__"
+   set "CSWITCH=/O2 /Gy- /EHsc /Zi  /c /W3 /MD"
+   set "FMACS=/D__EFL /DFORTRAN"
+   set "FSWITCH= /O2 /fpp /4Yportlib /auto /c /Fo.\ /MD /watch:source"
+
+   if exist *.F  (
+      rem  The "logo" is the banner that the compilers print-out, when they first
+      rem  start processing.  We only want to see that once, for each type of file
+      rem  we process.
+      rem
+      set "LOGOMAC="
+
+      for /F "usebackq tokens=*" %%P in ( `dir /B "*.F"` ) do (
+         echo.
+         echo. compiling Fortran file %%P
+         echo.
+         ifort !LOGOMAC! %CUSTMACROS% %FMACS% %FSWITCH% %%P >>compile.log  2>&1
+         set "LOGOMAC=/nologo"
+      )
+   )
+
+
+   if exist *.c  (
+      set "LOGOMAC="
+
+      for /F "usebackq tokens=*" %%P in ( `dir /B "*.c"` ) do (
+         echo.
+         echo. compiling C file %%P
+         echo.
+         cl !LOGOMAC! %CUSTMACROS% %CMACS% /D__MS_VC_INSTALL_PATH="%VCINPATH%" %CSWITCH% %%P >>compile.log 2>&1
+         set "LOGOMAC=/nologo"
+      )
+   )
+
+
+   if exist *.cpp  (
+      set "LOGOMAC="
+
+      for /F "usebackq tokens=*" %%P in ( `dir /B "*.cpp"` ) do (
+         echo.
+         echo. compiling C++ file %%P
+         echo.
+         rem cl !LOGOMAC! %CUSTMACROS% %CMACS% %CSWITCH% %%P >>compile.log 2>&1
+         cl !LOGOMAC! %CUSTMACROS% %CMACS% /D__MS_VC_INSTALL_PATH="%VCINPATH%" %CSWITCH% %%P >>compile.log 2>&1
+         set "LOGOMAC=/nologo"
+      )
+   )
+
+   if not exist compile.log (
+      echo.
+      echo. no local files to compile - is this what you wanted?
+      echo.
+   ) else (
+      findstr /I /C:": error" compile.log >compile_error.txt
+
+      if !errorlevel!==0 (
+         @echo off
+         echo.
+         echo.   ******************************************************************************
+         echo.
+         echo.     COMPILER ERROR!  CHECK compile.log FOR MORE INFORMATION
+         echo.
+         echo.
+         echo.   ******************************************************************************
+         echo.
+         endlocal
+         exit /B 1
+      )
+      del /Q compile_error.txt
+   )
+
+   endlocal
+   exit /B 0
+
+
+:LinkStuff
+   cd "%ACWORKDIR%"
+
+   if exist ANSYS.exe ( del /Q ANSYS.exe )
+
+   if "%AERO%" == "TRUE" (
+      type ansys.lrf >ansys.lrf.sav
+      echo "%ROOT_DIR%\ansys\Custom\User\%PLATFORM_DIR%\Aeroelastic\*.obj">>ansys.lrf
+
+      link @ansys.lrf 2>&1 |  findstr /V /R /c:"^ansyslib..lib.* LNK4099: PDB" /c:"^zlibwapi.lib.* LNK4099: PDB"
+      type ansys.lrf.sav >ansys.lrf
+      del /q ansys.lrf.sav
+
+   ) else (
+
+      link @ansys.lrf 2>&1 |  findstr /V /R /c:"^ansyslib..lib.* LNK4099: PDB" /c:"^zlibwapi.lib.* LNK4099: PDB"
+   )
+
+   if %ERRORLEVEL% GEQ 1 (
+      set "ltxt=LINK ERROR!"
+      set enum=1
+   ) else if not exist ansys.exe (
+      set "ltxt=LINK ERROR!"
+      set enum=1
+   ) else (
+      set "ltxt=Link was successful!"
+      set enum=0
+   )
+
+   echo.
+   echo.      ************************************************************************
+   echo.
+   echo.          %ltxt%
+   echo.
+   echo.      ************************************************************************
+   echo.
+
+   exit /B %enum%
+
+
+:CopyDlls
+   rem  We offer this, because some customers have multiple versions of ANSYS/MAPDL
+   rem  in their paths.  By copying libraries here, we guarantee that if they run
+   rem  this custom version, it will link with the correct version of the libraries.
+   rem
+   cd "%ACWORKDIR%"
+
+   echo.
+   echo.      ************************************************************************
+   echo.
+   echo.      The next question will give you the opportunity to copy the necessary
+   echo.      runtime DLLs.  Note, this only needs to be done once.  You can answer
+   echo.      "N" for all subsequent invocations of %0.
+   echo.
+   echo.      ************************************************************************
+   echo.
+
+:CD1
+   set /P "ANSW=Do you want to copy the runtime DLLs? (Y or N): "
+
+   if /I "%ANSW%"=="Y" (
+      copy /y "%ROOT_DIR%\ansys\Bin\%PLATFORM_DIR%\"*.dll .
+      copy /y "%ROOT_DIR%\commonfiles\AAS\bin\%PLATFORM_DIR%\"*.dll .
+      exit /B 0
+   ) else if /I "%ANSW%"=="N" (
+      exit /B 0
+   ) else (
+      echo.
+      echo "Please answer Y or N!"
+      echo.
+      goto CD1
+   )
+
+   exit /B 0
+
+
+:END
+endlocal
+
---
+++ bats/debug_ansusershared_v195.bat
@@ -0,0 +1,472 @@
+@echo off
+setlocal EnableDelayedExpansion
+cls
+
+rem   These need to change, for each new release.  You might need to change
+rem   the SetParameters function, if the compiler changes.  Otherwise, the
+rem   rest of the script should not need changing.
+rem
+set "revn=195"
+set "revndot=19.5"
+set "ROOT_DIR=%AWP_ROOT195%"
+set "IFORT_HOME=%IFORT_COMPILER17%"
+set "VSVER=vs2017"
+
+
+rem :::::::::::::::::::::::::::::::
+rem ::                           ::
+rem ::     E X E C U T I V E     ::
+rem ::                           ::
+rem :::::::::::::::::::::::::::::::
+
+call :CheckEnvVars        ||  goto :MyExit
+call :SetPlatformTarget   ||  goto :MyExit
+call :SetParameters       ||  goto :MyExit
+call :SetCompilerArgs     ||  goto :MyExit
+
+call :CompileSourceFiles  ||  goto :MyExit
+
+call :ShowBanner1
+
+:REPEAT
+call :ShowBanner2
+call :PromptForName  ||  goto :MyExit
+
+call :BuildCommonDll %SRCFILE%
+call :BuildOtherDll  %SRCFILE%
+goto :REPEAT
+
+endlocal
+goto :EOF
+
+
+rem :::::::::::::::::::::::::::::::
+rem ::                           ::
+rem ::     F U N C T I O N S     ::
+rem ::                           ::
+rem :::::::::::::::::::::::::::::::
+
+:CheckEnvVars
+rem  check some basic environment variables
+rem
+   set "TXT1="
+   set "TXT2="
+
+   if not defined AWP_ROOT195 (
+      set "TXT1=  I'm sorry, but environment variable AWP_ROOT%revn% does not exist."
+
+   ) else if "%AWP_ROOT195%"=="" (
+      set "TXT1=  I'm sorry, but environment variable AWP_ROOT%revn% exists, but is not set."
+
+   ) else if not exist "%AWP_ROOT195%\ansys\bin\winx64\ansys.exe" (
+      set "TXT1=  I'm sorry, but environment variable AWP_ROOT%revn% exists, and is set,"
+      set "TXT2=  but it does not seem to point to a useful location."
+   )
+
+   if not "%TXT1%"==""  (
+      echo.
+      echo. %TXT1%
+      if not "%TXT2%"==""  ( echo. %TXT2% )
+      echo.  It should be set to the install location, of the ANSYS software.  For
+      echo.  example:
+      echo.
+      echo.      C:\Program Files\ANSYS Inc\v%revn%
+      echo.
+      echo.  Please create/fix this variable, and then try again.
+      echo.
+      endlocal
+      exit /B 1
+   )
+   exit /B 0
+
+
+rem Nothing below this line needs to change, to revision this file
+
+:SetPlatformTarget
+   if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+      set "PLATFORM_DIR=winx64"
+      set "IFORT_PLATFORM=intel64"
+      set "MACHINE_TARGET=X64"
+
+   ) else if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
+      set "PLATFORM_DIR=winx64"
+      set "IFORT_PLATFORM=intel64"
+      set "MACHINE_TARGET=X64"
+
+   ) else if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+      set "PLATFORM_DIR=intel"
+      set "IFORT_PLATFORM=ia32"
+      set "MACHINE_TARGET=X86"
+   )
+   exit /B 0
+
+
+:SetParameters
+   rem "PLATFORM_DIR=winx64"
+   rem "IFORT_PLATFORM=intel64"
+   set "BIT_TARGET=64"
+
+   call "%IFORT_HOME%\bin\compilervars.bat" %IFORT_PLATFORM% %VSVER%
+
+rem  Some environment variables that the compiler will consult
+rem
+   set "AIDIR=%ROOT_DIR%\ansys\customize\Include"
+   set "ALDIR=%ROOT_DIR%\ansys\Custom\Lib\%PLATFORM_DIR%"
+
+   if exist "%AIDIR%\"  ( set "INCLUDE=%AIDIR%;%INCLUDE%" )
+   if exist "%ALDIR%\"  ( set "LIB=%ALDIR%;%LIB%" )
+
+   set /A FAIL_CNT    = 0
+   set /A SUCCESS_CNT = 0
+   exit /B 0
+
+
+:SetCompilerArgs
+   rem  command-line arguments, for the compiler lines
+   rem
+   set "COMMACS=/DNOSTDCALL /DARGTRAIL /DCADOE_ANSYS /DPCWINNT_SYS"
+   set "CMACS=/DCURVEFIT_EXPORTS /D_X86=1 /DOS_WIN32 /DWIN32 /D__STDC__"
+   set "FMACS=/D_EFL /DFORTRAN"
+   set "MACS64=/DPCWIN64_SYS /DPCWINX64_SYS"
+
+   set "COMSWITCH=/O2 /MD /c"
+   set "CSWITCH=/Gy- /EHsc /Zi /W3"
+   set "FSWITCH=/fpp /4Yportlib /auto /Fo.\ /watch:source"
+   exit /B 0
+
+
+:CompileSourceFiles
+   rem  Just build them all at once
+   del /q compile.log compile_error.txt >NUL 2>&1
+   del /q link.log    link_error.txt    >NUL 2>&1
+
+   if "%PLATFORM_DIR%"=="winx64" (
+      if exist *.F    ( ifort %COMMACS% %FMACS% %COMSWITCH% %FSWITCH% %MACS64% *.F   >>compile.log 2>&1 )
+      if exist *.c    ( cl    %COMMACS% %CMACS% %COMSWITCH% %CSWITCH% %MACS64% *.c   >>compile.log 2>&1 )
+      if exist *.cpp  ( cl    %COMMACS% %CMACS% %COMSWITCH% %CSWITCH% %MACS64% *.cpp >>compile.log 2>&1 )
+   )
+
+   if "%PLATFORM_DIR%"=="intel" (
+      if exist *.F    ( ifort %COMMACS% %FMACS% %COMSWITCH% %FSWITCH% /align:rec4byte *.F   >>compile.log 2>&1 )
+      if exist *.c    ( cl    %COMMACS% %CMACS% %COMSWITCH% %CSWITCH% /Zp4            *.c   >>compile.log 2>&1 )
+      if exist *.cpp  ( cl    %COMMACS% %CMACS% %COMSWITCH% %CSWITCH% /Zp4            *.cpp >>compile.log 2>&1 )
+   )
+
+   if exist compile.log (
+      FINDSTR /I /C:": error" compile.log >compile_error.txt
+      if !ERRORLEVEL!==0  (
+         call :ShowBanner "UPF COMPILER ERROR!  Check compile.log for more information."
+         exit /B 1
+      )
+
+      del /Q compile_error.txt
+   )
+
+   exit /B 0
+
+
+rem  We have a feature, that implements a "common block" data storage area,
+rem  for use by the DLLs.  If you are making use of this, then this must be
+rem  built first, so it can be linked with the other DLLs.
+rem
+:BuildCommonDll
+   set "SRC=%1"
+   set "UPFFILE=%SRC%Lib"
+   set "DLLFILE=%SRC%Lib.dll"
+
+   rem  bail, if this isn't the common block file
+   if /I not "%SRC%"=="userdata"  ( exit /B 0 )
+
+   call :CheckForSource %SRC% "common-block"  ||  exit /B 1
+   call :CheckForObject %SRC%                 ||  exit /B 1
+
+   rem  clean-up, and prep for linking
+   call :AssembleLinkerFile userdata
+
+   rem  link it, into its' own DLL
+   echo. ======================================== >> link.log
+   echo. Linking %SRC% ... >> link.log
+   link @%UPFFILE%.lrf >>     link.log 2>&1
+
+   call :CheckOnLink !ERRORLEVEL! %SRC% %DLLFILE%
+   echo.  >>                  link.log
+
+   rem  get rid of this, so any further links won't use it
+   del /q  userdata.obj >NUL 2>&1
+
+   exit /B 0
+
+
+rem  All other UPF DLLs are constructed here.
+rem
+:BuildOtherDll
+   set "SRC=%1"
+   set "UPFFILE=%SRC%Lib"
+   set "DLLFILE=%SRC%Lib.dll"
+
+   rem  bail, if they specified the common-block item
+   if /I "%SRC%"=="userdata"  ( exit /B 0 )
+
+   call :CheckForSource %SRC%     ||  exit /B 1
+   call :CheckForObject %SRC%     ||  exit /B 1
+
+   rem  Get rid of this, so any further links won't use it.
+   rem  Yes, this was already done, in BuildCommonDll.  Do
+   rem  it again, in case they tried to do things out-of-order.
+   del /q  userdata.obj >NUL 2>&1
+
+   rem  clean-up, and prep for linking
+   call :AssembleLinkerFile %SRC%
+
+   rem then link the DLL
+   echo. ======================================== >> link.log
+   echo. Linking %SRC% ... >> link.log
+   link @%SRC%Lib.lrf >>      link.log 2>&1
+
+   call :CheckOnLink !ERRORLEVEL! %SRC% %DLLFILE%
+   echo.  >>                  link.log
+
+   rem set ANS_USER_PATH=%CD%
+   rem echo. *** ANS_USER_PATH: %ANS_USER_PATH% ***
+
+   rem  get rid of these, or ansysNNN.exe will have a fit
+   rem del /q compile_error.txt link_error.txt >NUL 2>&1
+   exit /B 0
+
+
+rem  Check if the item they specified has a corresponding source file
+rem
+:CheckForSource
+   set "MYSRC=%1"
+   set "TXT=%2"
+
+   rem  bail, if no source file
+   if exist "%MYSRC%.F" (
+      exit /B 0
+   ) else if exist "%MYSRC%.c" (
+      exit /B 0
+   ) else if exist "%MYSRC%.cpp" (
+      exit /B 0
+   ) else (
+      echo.
+      echo. Sorry, the %TXT% source file, for %MYSRC%, is not here.
+      echo.
+      pause
+      exit /B 1
+   )
+
+   exit /B 0
+
+
+rem  Check if the item they specified compiled successfully
+rem
+:CheckForObject
+   set "MYSRC=%1"
+
+   rem  bail, if no object file
+   if not exist "%MYSRC%.obj"    (
+      echo.
+      echo. Sorry, the source file, for %SRC%, did not compile.
+      echo. See the file  compile.log, for compiler warnings and errors.
+      echo.
+      pause
+      exit /B 1
+   )
+
+   exit /B 0
+
+
+:CheckOnLink
+   set /A "errlvl = %1"
+   set "mysrc=%2"
+   set "dll=%3"
+
+   if %errlvl% GEQ 1  (
+      call :ShowBanner "%dll% (%mysrc%) has FAILED to link"
+      set /A FAIL_CNT += 1
+      exit /B 1
+   ) else if not exist "%dll%" (
+      call :ShowBanner "%dll% (%mysrc%) has FAILED to link"
+      set /A FAIL_CNT += 1
+      exit /B 1
+   )
+
+   call :ShowBanner "%dll% (%mysrc%) has been successfully built."
+   set /A SUCCESS_CNT += 1
+   exit /B 0
+
+
+:AssembleLinkerFile
+   rem  clean old linker files, and create new ones
+   set "SRC=%1"
+   set "UPFFILE=%SRC%Lib"
+
+   del /q  %UPFFILE%.lib %UPFFILE%.dll %UPFFILE%.lrf %UPFFILE%.map %UPFFILE%.def %UPFFILE%.exp >NUL 2>&1
+
+   rem  The source file was already compiled, so just create
+   rem  the exported-functions file, and the linker resource
+   rem  file.
+   set "EXFILE=%UPFFILE%ex.def"
+   set "LRFFILE=%UPFFILE%.lrf"
+
+   if /I not "%SRC%" == "userdata"  (
+      echo EXPORTS> %EXFILE%
+      echo.  >>     %EXFILE%
+      echo %SRC%>>  %EXFILE%
+      "%ROOT_DIR%\ansys\Custom\user\%PLATFORM_DIR%\upcase" %EXFILE%
+   )
+
+   echo -out:%UPFFILE%.dll>              %LRFFILE%
+   if /I not "%SRC%" == "userdata"  (
+      echo -def:%EXFILE%>>               %LRFFILE%
+   )
+   echo -dll>>                           %LRFFILE%
+   echo -machine:%MACHINE_TARGET%>>      %LRFFILE%
+   echo -map>>                           %LRFFILE%
+   echo -manifest:embed>>                %LRFFILE%
+   echo -defaultlib:ANSYS.lib>>          %LRFFILE%
+   if exist "userdataLib.lib"  (
+      echo -defaultlib:userdataLib.lib>> %LRFFILE%
+   )
+   echo. >>                              %LRFFILE%
+   if "%SRC%" == "userdata"  (
+      echo userdata.obj>>                %LRFFILE%
+   ) else (
+      echo *.obj>>                       %LRFFILE%
+   )
+
+   exit /B 0
+
+
+rem :::::::::::::::::::::::::::::::::::::::::::::::::
+rem ::                                             ::
+rem ::     Human - Machine Interface Functions     ::
+rem ::                                             ::
+rem :::::::::::::::::::::::::::::::::::::::::::::::::
+
+:ShowBanner1
+   echo.
+   echo.    This is the Mechanical APDL %revndot% ANSUSERSHARED script.  It is used
+   echo.    to build a DLL of User Programmable Features, for the M-APDL program.
+   echo.
+   echo.
+   echo.          NOTE:  The user subroutine source file^(s^) are expected to
+   echo.          reside in this directory.
+   echo.
+   exit /B 0
+
+:ShowBanner2
+   cls
+   echo.
+   echo. ANSYS now has a "common block" feature, so that DLLs can share
+   echo. data between themselves.  If you want to use this, then select
+   echo.        USERDATA
+   echo. first, before any other selection.
+   echo.
+   echo.
+   echo. Enter one of the following choices to create your
+   echo. user-programmable feature DLL:
+   echo.
+   echo.   UANBEG, UANFIN, UCNVRG, UELMATX, UITBEG, UITFIN, ULDBEG, ULDFIN,
+   echo.   USER_TBELASTIC, USER01, USER02, USER03, USER04, USER05, USER06,
+   echo.   USER07, USER08, USER09, USER10, USERCNPROP USERCR, USERCREEP,
+   echo.   USERCV, USERCZM, USERDATA, USERELEM, USERFLD, USERFREESTRAIN,
+   echo.   USERFRIC, USERFX, USERHYPER, USERINISTATE, USERINTER, USERMAT,
+   echo.   USERMATTH, USEROU, USERSWSTRAIN, USERTHSTRAIN, USERWEAR, USOLBEG,
+   echo.   USOLFIN, USREFL, USRSHIFT, USRSURF116, USSBEG, USSFIN, UTIMEINC,
+   echo.
+   echo. Enter Carriage-Return (Enter^) to Quit.
+   echo.
+   echo. Enter a user-programmable feature source filename, without the file
+   echo. extension.  For example: USERMAT or usermat.  The filename is case-
+   echo. insensitive.
+   echo.
+   exit /B 0
+
+
+:PromptForName
+   set "SRCFILE="
+   set "UPFFILE="
+   set "DLLFILE="
+   set /P "SRCFILE=Enter a user-programmable feature source filename: "
+
+   rem trim any unneeded whitespace
+   rem
+rem   SetLocal EnableDelayedExpansion
+rem   set Params=%SRCFILE%
+rem   for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
+
+rem   set "UPFFILE=%SRCFILE%Lib"
+   if "%SRCFILE%"==""  ( goto :EOF )
+
+rem   if not exist %SRCFILE%.F (
+rem      echo.
+rem      echo. %SRCFILE%.F Does Not Exist!
+rem      echo.
+rem      pause
+rem   )
+   exit /B 0
+
+
+:ShowBanner
+   echo.
+   echo.   ************************************************************************
+   echo.
+   echo.       %1
+   echo.
+   echo.   ************************************************************************
+   echo.
+   exit /B 0
+
+
+:MyExit
+   set /A TTL = %FAIL_CNT% + %SUCCESS_CNT%
+
+   echo.
+   echo. Summary:
+   echo.   Total items attempted: %TTL%
+   echo.   Successful builds:     %SUCCESS_CNT%
+   echo.   Failed     builds:     %FAIL_CNT%
+   echo.
+
+   if %TTL% equ 0  (
+      endlocal
+      goto :eof
+   )
+
+   if %FAIL_CNT% geq 1  (
+      echo. Please fix the build failures, and then run this script again.
+      echo.
+
+      endlocal
+      goto :eof
+   )
+
+   if %SUCCESS_CNT% geq 1  (
+      echo.
+      echo.   ************************************************************************
+      echo.
+      echo.   Set environment variable ANS_USER_PATH, to the directory where the
+   )
+
+   if %SUCCESS_CNT% equ 1  (
+      echo.   new DLL file resides.  Then run ansys%revn%, to use your newly-
+      echo.   generated user shared library.
+
+   ) else if %SUCCESS_CNT% geq 2  (
+      echo.   new DLL files reside.  Then run ansys%revn%, to use your newly-
+      echo.   generated user shared libraries.
+   )
+
+   if %SUCCESS_CNT% geq 1  (
+      echo.
+      echo.   ************************************************************************
+      echo.
+   )
+
+   rem  if nothing worked, then be silent
+
+   endlocal
+   goto :eof
+
+
